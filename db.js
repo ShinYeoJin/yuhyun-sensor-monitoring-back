38f07147f23f48c68da4cfae48151e72807acc51
신여진 <ATIV1@MacBook-Pro.local>
2026-02-22 03:51:47 +0900

    third test

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false } // Render Free에서 RDS 접속용
 });
 
 module.exports = pool;
